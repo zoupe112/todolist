a9ff7ab83e8695053dc87deac7badaa1f4434515
zoupe112 <zoupe112@gmail.com>
2023-10-16 04:43:57 +0700

    todolist

--- src/app/page.js
+++ src/app/page.js
@@ -54,47 +54,36 @@ export default function Home() {
 
   return (
     <>
-      <body className='body' style={{ backgroundImage: "url(/background-react.png)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', fontFamily: 'inherit' }
+      <div className='bg-blue-500 flex justify-center flex items-center bg-gradient-to-r from-cyan-500 to-blue-500' style={{ fontFamily: 'inherit', height: '100vh', textAlign: 'center', fontWeight: 'bold' }
       }>
-        <div className='flex-container'>
-          <div style={{ backgroundColor: 'rgba(255,255,255,0.2)', borderRadius: '50px', padding: '50px', backdropFilter: 'blur(5px)' }}>
-            <div>
-              <h1 style={{ fontSize: '48px', color: 'white', animation: 'ease-in-out' }}>Login</h1>
-              <br></br>
-              <div>
-                <input type="text" name="name" className='input'
-                  value={name} placeholder='Username'
-                  onChange={(e) => {
-                    console.log(e.target.value)
-                    let input = e.target.value
-                    setName(input)
-                  }}
-                  style={{ outline: 'none' }}
-                />
-                {nameError == null ? null : nameError}
-                <div>
-                  <br></br>
-                  <input type='text' name="Password" className='input'
-                    placeholder='Password'
-                    value={password}
-                    onChange={(e) => {
-                      let input = e.target.value
-                      setPassword(input)
-                    }}
-                    style={{ outline: 'none' }}
-                  />
-                </div>
-              </div>
-              <div>
-                <br></br>
-                {passwordError == null ? null : passwordError}
-                <button onClick={() => submit()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' style={{ textAlign: '' }}>save</button><br></br><br></br>
-                <button onClick={() => router.push('/page2')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' >page 2</button>
-              </div>
-            </div>
-          </div>
+        <div style={{ backgroundColor: 'rgba(255,255,255,0.2)', borderRadius: '50px', padding: '50px', backdropFilter: 'blur(5px)' }}>
+          <h1 style={{ fontSize: '48px', color: 'white' }}>Login</h1>
+          <br></br>
+          <input class type="text" name="name" className='input rounded-md'
+            value={name} placeholder='Username'
+            onChange={(e) => {
+              console.log(e.target.value)
+              let input = e.target.value
+              setName(input)
+            }}
+            style={{ outline: 'none' }}
+          />
+          {nameError == null ? null : nameError}
+          <br></br><br></br>
+          <input type='text' name="Password" className='input rounded-md'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => {
+              let input = e.target.value
+              setPassword(input)
+            }}
+            style={{ outline: 'none' }}
+          />
+          <br></br><br></br>
+          {passwordError == null ? null : passwordError}
+          <button onClick={() => submit()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded-md'>Login</button><br></br><br></br>
         </div>
-      </body >
+      </div>
     </>
   )
 }
--- src/app/page2/page.js
+++ src/app/page2/page.js
@@ -85,53 +85,50 @@ function App() {
     }
 
     return (
-        <body style={{ backgroundImage: 'url(/background-react.png)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', fontSize: '20px', fontFamily: 'inherit' }}>
-            <div className='flex justify-center'>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center" style={{ fontSize: '20px', fontFamily: 'inherit', height: '100vh' }}>
+            <div>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <h1 style={{ textAlign: 'center', fontSize: '36px', fontWeight: 'bold', color: 'white' }}>TodoList</h1><br></br>
+                <div style={{ backgroundColor: 'white', padding: '20px 40px', borderRadius: '10px', margin: '10px' }}>
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)
+                        } placeholder="Add todo"
+                        style={{ height: '40px', width: '500px', outline: 'none', borderBottom: '2px solid #E8E6E6' }}
+                    />&nbsp;&nbsp;
+                    <button onClick={() => addTodo(input)} className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300" style={{ borderRadius: '10px', outline: 'none' }}>Add</button>
+                </div>
+                <br></br>
                 <div>
-                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-                    <div style={{ textAlign: 'center', fontSize: '36px', fontWeight: 'bold' }}>TodoList</div><br></br>
-                    <div>
-                        <div style={{ backgroundColor: 'white', padding: '20px 40px', borderRadius: '10px', margin: '10px' }}>
-                            <input
-                                type="text"
-                                value={input}
-                                onChange={(e) => setInput(e.target.value)
-                                } placeholder="Add todo"
-                                style={{ height: '40px', width: '500px', outline: 'none', borderBottom: '2px solid #E8E6E6' }}
-                            />&nbsp;&nbsp;
-                            <button onClick={() => addTodo(input)} className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300" style={{ borderRadius: '10px', outline: 'none' }}>Add</button>
-                        </div>
-                        <br></br>
-                        <div>
-                            <ul>
-                                {list.map((obj, index) => (
-                                    <li key={obj.id} style={{ backgroundColor: 'white', borderRadius: '10px', padding: '10px 10px', margin: '10px' }}>
-                                        {updateindex === index ?
-                                            <div className="flex justify-between pl-7">
-                                                <input
-                                                    type="text"
-                                                    value={updateinput}
-                                                    onChange={(e) => setUpdateinput(e.target.value)}
-                                                    style={{ outline: 'none', display: 'flex', alignItems: 'center' }} />
-                                                <button onClick={() => savetodo(obj.id)}><MdSaveAs size='2em' style={{ display: 'inline-block', color: 'white', backgroundColor: 'green', padding: '5px', borderRadius: '10px' }}></MdSaveAs>
-                                                </button>
-                                            </div> : <div className="flex justify-between pl-7">
-                                                <div style={{ outline: 'none', display: 'flex', alignItems: 'center' }}>{obj.todo}</div>
-                                                <div>
-                                                    <button onClick={() => deleteTodo(obj.id)}>&nbsp;&nbsp;<AiFillDelete size='2em' style={{ display: 'inline-block', color: 'white', backgroundColor: 'red', padding: '5px', borderRadius: '10px' }}></AiFillDelete>
-                                                    </button> &nbsp;&nbsp;&nbsp;
-                                                    <button onClick={() => updatetodo(obj.todo, index)}><FaRegEdit size='2em' className="bg-blue-500" style={{ display: 'inline-block', color: 'white', padding: '5px', borderRadius: '10px' }}></FaRegEdit>
-                                                    </button>
-                                                </div>
-                                            </div>}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
+                    <ul>
+                        {list.map((obj, index) => (
+                            <li key={obj.id} style={{ backgroundColor: 'white', borderRadius: '10px', padding: '10px 10px', margin: '10px', }}>
+                                {updateindex === index ?
+                                    <div className="flex justify-between pl-7" style={{ alignItems: 'center' }}>
+                                        <input
+                                            type="text"
+                                            value={updateinput}
+                                            onChange={(e) => setUpdateinput(e.target.value)}
+                                            style={{ outline: 'none', display: 'flex', alignItems: 'center', borderBottom: '2px solid #E8E6E6', width: '81%' }} />
+                                        <button onClick={() => savetodo(obj.id)}><MdSaveAs size='2em' style={{ display: 'inline-block', color: 'white', backgroundColor: 'green', padding: '5px', borderRadius: '10px' }}></MdSaveAs>
+                                        </button>
+                                    </div>
+                                    : <div className="flex justify-between pl-7" style={{ alignItems: 'center' }}>
+                                        <div style={{ borderBottom: '2px solid #E8E6E6', width: '81%' }}>{obj.todo}</div>
+                                        <div>
+                                            <button onClick={() => deleteTodo(obj.id)}>&nbsp;&nbsp;<AiFillDelete size='2em' style={{ display: 'inline-block', color: 'white', backgroundColor: 'red', padding: '5px', borderRadius: '10px' }}></AiFillDelete>
+                                            </button> &nbsp;&nbsp;&nbsp;
+                                            <button onClick={() => updatetodo(obj.todo, index)}><FaRegEdit size='2em' className="bg-blue-500" style={{ display: 'inline-block', color: 'white', padding: '5px', borderRadius: '10px' }}></FaRegEdit>
+                                            </button>
+                                        </div>
+                                    </div>}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
-        </body >
+        </div >
     );
 
 }
